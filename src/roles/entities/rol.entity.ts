095a5822ae9cfbd489bb5a3278e8142d5cf2af0e
PamelaCanaza <lunapamela886@gmail.com>
2025-11-13 05:27:41 -0400

    feat(rol): crear entidad Rol con sus columnas básicas

---
+++ src/roles/entities/rol.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("ROL")
+export class Rol {
+  @PrimaryGeneratedColumn({ name: "idRol" })
+  idRol: number;
+
+  @Column({ name: "nombreRol", unique: true, length: 50 })
+  nombreRol: string;
+
+  @Column({ name: "descripcion", type: "varchar", length: 200, nullable: true })
+  descripcion?: string;
+}
