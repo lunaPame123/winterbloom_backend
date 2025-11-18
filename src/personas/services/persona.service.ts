5b505f0fd115ad242728faec749234964aeea715
PamelaCanaza <lunapamela886@gmail.com>
2025-11-18 19:14:08 -0400

    feat(persona): agregar metodo crear en personaservice con validación de CI

--- src/personas/services/persona.service.ts
+++ src/personas/services/persona.service.ts
@@ -1,27 +1,10 @@
 import { PersonaRepository } from "../repositories/persona.repository";
-import { Persona } from "../entities/persona.entity";
 
 export class PersonaService {
-  private repository = new PersonaRepository();
+  async crear(data: any){
+    const existe = await PersonaRepository.buscarPorCi(data.ci);
+    if (existe) throw new Error ("El CI ya esta registrado");
 
-  async getAll(): Promise<Persona[]> {
-    return this.repository.findAll();
-  }
-
-  async create(data: Partial<Persona>): Promise<Persona> {
-    // Validación simple: correo único
-    const exists = await this.repository.findByCorreo(data.correo!);
-    if (exists) throw new Error("El correo ya está registrado");
-
-    const persona = new Persona();
-    persona.nombre = data.nombre!;
-    persona.apellidoPaterno = data.apellidoPaterno!;
-    persona.apellidoMaterno = data.apellidoMaterno!;
-    persona.ci = data.ci!;
-    persona.correo = data.correo!;
-    persona.rol = data.rol!;
-    persona.setPassword(data.contraseña!);
-
-    return this.repository.createAndSave(persona);
+    return await PersonaRepository.crearPersona(data);
   }
 }
