58fc8bb5254146708b700efc4b9c3a215f6219f6
PamelaCanaza <lunapamela886@gmail.com>
2025-11-18 19:16:19 -0400

    feat(persona): agregar metodo listar en PersonaService

--- src/personas/services/persona.service.ts
+++ src/personas/services/persona.service.ts
@@ -1,10 +1,14 @@
 import { PersonaRepository } from "../repositories/persona.repository";
 
 export class PersonaService {
-  async crear(data: any){
+  async crearPersonas(data: any){
     const existe = await PersonaRepository.buscarPorCi(data.ci);
     if (existe) throw new Error ("El CI ya esta registrado");
 
     return await PersonaRepository.crearPersona(data);
+  }
+
+  async listarPersonas(){
+    return await PersonaRepository.listarConUsuarios();
   }
 }
