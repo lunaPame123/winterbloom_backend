42356effd0f23cb3d353c1f7bbb5ea0c956ec555
PamelaCanaza <lunapamela886@gmail.com>
2025-11-18 21:13:23 -0400

    feat(routes): conectar PersonaRoutes al router principal

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,8 @@
 import { Router } from "express";
-import { RolController } from "../roles/controllers/rol.controller";
-import { PersonaController } from "../personas/controllers/persona.controller";
+import personaRoutes from "../personas/routes/persona.routes";
 
 const router = Router();
-const rolCtrl = new RolController();
-const personaCtrl = new PersonaController();
 
-// Rutas ROL
-router.get("/roles", (req, res) => rolCtrl.getRoles(req, res));
-router.post("/roles", (req, res) => rolCtrl.createRol(req, res));
-
-// Rutas PERSONA
-router.get("/personas", (req, res) => personaCtrl.getPersonas(req, res));
-router.post("/personas", (req, res) => personaCtrl.createPersona(req, res));
-
-router.get("/", (req, res) => res.json({ message: "API WinterBloom activa" }));
+router.use("/personas", personaRoutes);
 
 export default router;
