10ccb17f7b5e1764fa825716b79bf0bf36920128
PamelaCanaza <lunapamela886@gmail.com>
2025-11-18 23:15:07 -0400

    feat(rol): modificar RolController con endpoints para roles

--- src/roles/controllers/rol.controller.ts
+++ src/roles/controllers/rol.controller.ts
@@ -4,25 +4,21 @@ import { RolService } from "../services/rol.service";
 const service = new RolService();
 
 export class RolController {
-  async getRoles(req: Request, res: Response) {
+  static async crear( req: Request, res: Response){
     try {
-      const roles = await service.getAll();
-      res.json(roles);
-    } catch (err) {
-      res.status(500).json({ message: (err as Error).message });
+      const rol = await service.crear(req.body);
+      res.json(rol);
+    } catch (error: any){
+      res.status(400).json({ error: error.message});
     }
   }
 
-  async createRol(req: Request, res: Response) {
+  static async listar(req: Request, res: Response){
     try {
-      const { nombreRol, descripcion } = req.body;
-      if (!nombreRol)
-        return res.status(400).json({ message: "nombreRol es requerido" });
-
-      const rol = await service.create({ nombreRol, descripcion });
-      res.status(201).json(rol);
-    } catch (err) {
-      res.status(400).json({ message: (err as Error).message });
+      const roles = await service.listar();
+      res.json(roles);
+    } catch (error: any){
+      res.status(400).json({ error: error.message });
     }
   }
 }
