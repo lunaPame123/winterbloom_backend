8317597342618ea1419f725a0f167f8da82b3a25
PamelaCanaza <lunapamela886@gmail.com>
2025-11-18 22:47:40 -0400

    feat(rol): crear entidad asociativa RolUsuario

---
+++ src/roles/entities/rolusuario.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from "typeorm";
+import { Usuario } from "../../usuarios/entities/usuario.entity";
+import { Rol } from "./rol.entity";
+
+@Entity({ name: "rolusuario"})
+export class RolUsuario {
+    @PrimaryGeneratedColumn()
+    idUsuarioRol!: number;
+
+    @ManyToOne(() => Usuario, usuario => usuario.roles)
+    @JoinColumn({ name: "idUsuario" })
+    usuario!: Usuario;
+
+    @ManyToOne(() => Rol, rol => rol.usuarios)
+    @JoinColumn({ name: "idRol" })
+    rol: Rol;
+}
