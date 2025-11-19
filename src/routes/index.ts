37b54b1289aa4514de0248ff6d666135833dfbe0
PamelaCanaza <lunapamela886@gmail.com>
2025-11-18 22:34:02 -0400

    feat(routes): conectar UsuarioRoutes al router principal

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import personaRoutes from "../personas/routes/persona.routes";
+import usuarioRoutes from "../usuarios/routes/usuario.routes";
 
 const router = Router();
 
 router.use("/personas", personaRoutes);
+router.use("/usuarios", usuarioRoutes);
 
 export default router;
