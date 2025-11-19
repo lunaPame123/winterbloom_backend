82a10780dfa3642abbe01d34b88d69fdbd7731f1
PamelaCanaza <lunapamela886@gmail.com>
2025-11-18 20:44:29 -0400

    feat(persona): agregar metodo crear en PersonaController

--- src/personas/controllers/persona.controller.ts
+++ src/personas/controllers/persona.controller.ts
@@ -4,25 +4,12 @@ import { PersonaService } from "../services/persona.service";
 const service = new PersonaService();
 
 export class PersonaController {
-  async getPersonas(req: Request, res: Response) {
-    try {
-      const personas = await service.getAll();
-      res.json(personas);
-    } catch (err) {
-      res.status(500).json({ message: (err as Error).message });
+    static async crear (req: Request, res: Response){
+        try {
+            const persona = await service.crearPersonas(req.body);
+            res.json(persona);
+        } catch(error: any){
+            res.status(400).json({error: error.message});
+        }
     }
-  }
-
-  async createPersona(req: Request, res: Response) {
-    try {
-      const data = req.body;
-      if (!data.correo || !data.contraseña)
-        return res.status(400).json({ message: "Correo y contraseña son obligatorios" });
-
-      const persona = await service.create(data);
-      res.status(201).json(persona);
-    } catch (err) {
-      res.status(400).json({ message: (err as Error).message });
-    }
-  }
 }
