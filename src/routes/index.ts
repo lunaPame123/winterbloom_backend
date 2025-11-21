d536e1a711839f462403a9c72b818df5981842a1
PamelaCanaza <lunapamela886@gmail.com>
2025-11-21 00:35:24 -0400

    feat(routes): conectar rutas de Arreglos al router principal

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import personaRoutes from "../personas/routes/persona.routes";
 import usuarioRoutes from "../usuarios/routes/usuario.routes";
 import rolRoutes from "../roles/routes/rol.routes";
 import rolUsuarioRoutes from "../roles/routes/rolusuario.routes";
+import arregloRoutes from "../arreglos/routes/arreglo.routes";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/personas", personaRoutes);
 router.use("/usuarios", usuarioRoutes);
 router.use("/roles", rolRoutes);
 router.use("/roles-usuario", rolUsuarioRoutes);
+router.use("/arreglos", arregloRoutes);
 
 export default router;
