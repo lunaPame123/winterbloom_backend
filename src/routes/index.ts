179ccdc1f3cf6e609ded33144f102eeb024c0496
PamelaCanaza <lunapamela886@gmail.com>
2025-11-21 01:06:30 -0400

    feat(routes): conectar rutas de Flores con el router principal

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import usuarioRoutes from "../usuarios/routes/usuario.routes";
 import rolRoutes from "../roles/routes/rol.routes";
 import rolUsuarioRoutes from "../roles/routes/rolusuario.routes";
 import arregloRoutes from "../arreglos/routes/arreglo.routes";
+import florRoutes from "../flores/routes/flor.routes";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/usuarios", usuarioRoutes);
 router.use("/roles", rolRoutes);
 router.use("/roles-usuario", rolUsuarioRoutes);
 router.use("/arreglos", arregloRoutes);
+router.use("/flores", florRoutes);
 
 export default router;
