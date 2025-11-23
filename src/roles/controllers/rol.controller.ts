d11cfbbab159bbe4ff47e84bc1ed2289850102a9
PamelaCanaza <lunapamela886@gmail.com>
2025-11-23 05:16:51 -0400

    feat(roles): agregar las nuevas rutas al controlador con validacion de errores

--- src/roles/controllers/rol.controller.ts
+++ src/roles/controllers/rol.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { RolService } from "../services/rol.service";
 
-const service = new RolService();
+const rolService = new RolService();
 
 export class RolController {
   static async crear( req: Request, res: Response){
     try {
-      const rol = await service.crear(req.body);
-      res.json(rol);
+      const nuevoRol = await rolService.crear(req.body);
+      res.json(nuevoRol);
     } catch (error: any){
       res.status(400).json({ error: error.message});
     }
@@ -15,9 +15,39 @@ export class RolController {
 
   static async listar(req: Request, res: Response){
     try {
-      const roles = await service.listar();
+      const roles = await rolService.listar();
       res.json(roles);
     } catch (error: any){
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  static async obtener(req: Request, res: Response) {
+    try {
+      const idRol = parseInt(req.params.idRol);
+      const rol = await rolService.obtenerPorId(idRol);
+      res.json(rol);
+    } catch (error: any) {
+      res.status(404).json({ error: error.message });
+    }
+  }
+
+  static async actualizar(req: Request, res: Response) {
+    try {
+      const idRol = parseInt(req.params.idRol);
+      const rol = await rolService.actualizar(idRol, req.body);
+      res.json(rol);
+    } catch (error: any) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  static async eliminar(req: Request, res: Response) {
+    try {
+      const idRol = parseInt(req.params.idRol);
+      await rolService.eliminar(idRol);
+      res.json({ mensaje: "Rol eliminado (lógicamente)" });
+    } catch (error: any) {
       res.status(400).json({ error: error.message });
     }
   }
