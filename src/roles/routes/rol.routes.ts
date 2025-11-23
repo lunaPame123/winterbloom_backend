b13c519a298f9f003a5c863e83a8244bd4ffeaec
PamelaCanaza <lunapamela886@gmail.com>
2025-11-23 05:17:45 -0400

    feat(roles): completar CRUD en routes con endpoints

--- src/roles/routes/rol.routes.ts
+++ src/roles/routes/rol.routes.ts
@@ -4,6 +4,9 @@ import { RolController } from "../controllers/rol.controller";
 const router = Router();
 
 router.get("/", RolController.listar);
+router.get("/:idRol", RolController.obtener);
 router.post("/", RolController.crear);
+router.put("/:idRol", RolController.actualizar);
+router.delete("/:idRol", RolController.eliminar);
 
 export default router;
