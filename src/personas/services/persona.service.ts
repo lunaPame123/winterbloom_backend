ba3434de53adfb01eeb4cb924979c6bc16a5865a
PamelaCanaza <lunapamela886@gmail.com>
2025-11-23 01:35:48 -0400

    fix: mejorar PersonaService, tipado y nombres descriptivos

--- src/personas/services/persona.service.ts
+++ src/personas/services/persona.service.ts
@@ -1,27 +1,28 @@
 import { PersonaRepository } from "../repositories/persona.repository";
+import { Persona } from "../entities/persona.entity";
 
 export class PersonaService {
-  async crearPersonas(data: any){
-    const existe = await PersonaRepository.buscarPorCi(data.ci);
-    if (existe) throw new Error ("El CI ya esta registrado");
+  async crearPersona(data: Partial<Persona>){
+    const existe = await PersonaRepository.buscarPorCi(data.ci!);
+    if (existe) throw new Error ("La persona con este carnet de identidad ya esta registrada");
 
-    return await PersonaRepository.crearPersona(data);
+    return PersonaRepository.crearPersona(data);
   }
 
   async listarPersonas(){
-    return await PersonaRepository.listarConUsuarios();
+    return PersonaRepository.listarConUsuarios();
   }
 
-  async obtenerPersonaId(idPersona: number){
+  async obtenerPersonaPorId(idPersona: number){
     const persona = await PersonaRepository.buscarPorId(idPersona);
     if (!persona) throw new Error("Persona no encontrada");
     return persona;
   }
 
-  async actualizarPersona (idPersona: number, data: any){
+  async actualizarPersona (idPersona: number, data: Partial<Persona>) {
     const persona = await PersonaRepository.buscarPorId(idPersona);
     if (!persona) throw new Error ("Persona no encontrada");
 
-    return await PersonaRepository.actualizarPersona(idPersona, data);
+    return PersonaRepository.actualizarPersona(idPersona, data);
   }
 }
