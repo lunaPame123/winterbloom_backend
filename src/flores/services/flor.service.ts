5cafd10cd0905ea7033b7b62267d28caa6ad04d5
PamelaCanaza <lunapamela886@gmail.com>
2025-11-23 06:45:04 -0400

    Refactor (Flor): definir  metodos claros, validaciones de datos y manejo de errores consistente en el servidor

--- src/flores/services/flor.service.ts
+++ src/flores/services/flor.service.ts
@@ -3,16 +3,16 @@ import { Flor } from "../entities/flor.entity";
 
 export class FlorService {
 
-  async obtenerTodos() {
+  async obtenerTodas() {
     return await FlorRepository.obtenerTodasLasFlores();
   }
 
-  async obtenerActivos() {
-    return await FlorRepository.obtenerFloresActivos();
+  async obtenerActivas() {
+    return await FlorRepository.obtenerFloresActivas();
   }
 
-  async obtenerPorId(id: number) {
-    const flor = await FlorRepository.obtenerFloresPorId(id);
+  async obtenerPorId(idFlor: number) {
+    const flor = await FlorRepository.obtenerFlorPorId(idFlor);
     if (!flor) {
       throw new Error("La flor no existe");
     }
@@ -23,25 +23,25 @@ export class FlorService {
     return await FlorRepository.buscarFloresPorColor(color);
   }
 
-  async buscarPorNombre(nombre: string) {
-    return await FlorRepository.buscarFloresPorNombre(nombre);
+  async buscarPorNombre(nombreFlor: string) {
+    return await FlorRepository.buscarFloresPorNombre(nombreFlor);
   }
 
-  async crear(data: Partial<Flor>) {
-    if (!data.nombre) throw new Error("El nombre de la flor es obligatorio");
-    if (!data.precio || data.precio <= 0) throw new Error("El precio debe ser mayor a cero");
-    if (!data.color) throw new Error("El color es obligatorio");
+  async crear(datosFlor: Partial<Flor>) {
+    if (!datosFlor.nombreFlor) throw new Error("El nombre de la flor es obligatorio");
+    if (!datosFlor.precio || datosFlor.precio <= 0) throw new Error("El precio debe ser mayor a cero");
+    if (!datosFlor.color) throw new Error("El color es obligatorio");
 
-    return await FlorRepository.crearFlor(data);
+    return await FlorRepository.crearFlor(datosFlor);
   }
 
-  async actualizar(id: number, data: Partial<Flor>) {
-    await this.obtenerPorId(id);
-    return await FlorRepository.actualizarFlor(id, data);
+  async actualizar(idFlor: number, datosFlor: Partial<Flor>) {
+    await this.obtenerPorId(idFlor);
+    return await FlorRepository.actualizarFlor(idFlor, datosFlor);
   }
 
-  async eliminacionLogica(id: number) {
-    await this.obtenerPorId(id);
-    return await FlorRepository.eliminarFlores(id);
+  async eliminacionLogica(idFlor: number) {
+    await this.obtenerPorId(idFlor);
+    return await FlorRepository.eliminarFlor(idFlor);
   }
 }
