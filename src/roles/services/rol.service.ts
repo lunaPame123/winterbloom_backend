6ed6882a152819890b6d6187e3b15d74cca107cd
PamelaCanaza <lunapamela886@gmail.com>
2025-11-23 05:15:21 -0400

    feat(roles): agregar validaciones y metodos faltantes en rol.service

--- src/roles/services/rol.service.ts
+++ src/roles/services/rol.service.ts
@@ -1,11 +1,28 @@
+import { Rol } from "../entities/rol.entity";
 import { RolRepository } from "../repositories/rol.repository";
 
 export class RolService {
-  async crear(data: any){
+  async crear(data: Partial<Rol>){
     return await RolRepository.crearRol(data);
   }
 
   async listar(){
     return await RolRepository.listarRoles();
+  }
+
+  async obtenerPorId(idRol: number) {
+    const rol = await RolRepository.obtenerRolPorId(idRol);
+    if (!rol) throw new Error("El rol no existe");
+    return rol;
+  }
+
+  async actualizar(idRol: number, data: Partial<Rol>) {
+    await this.obtenerPorId(idRol); 
+    return await RolRepository.actualizarRol(idRol, data);
+  }
+
+  async eliminar(idRol: number) {
+    await this.obtenerPorId(idRol);
+    return await RolRepository.eliminarRol(idRol);
   }
 }
