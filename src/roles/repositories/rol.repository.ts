5d1ebf96fb2303c959de4b4006b97ff75ff3f368
PamelaCanaza <lunapamela886@gmail.com>
2025-11-23 05:14:18 -0400

    refactor(roles): renombrar metodos del rol.repository para mayor claridad y agregar CRUD completo

--- src/roles/repositories/rol.repository.ts
+++ src/roles/repositories/rol.repository.ts
@@ -5,13 +5,32 @@ export const RolRepository = AppDataSource.getRepository(Rol).extend({
 
   async crearRol(data: Partial<Rol>) {
     const existe = await this.findOne({ where: { nombreRol: data.nombreRol } });
-    if (existe) throw new Error("El Rol ya existe");
+    if (existe) throw new Error("El nombre del rol ya está registrado");
 
-    const rol = this.create(data);
-    return await this.save(rol);
+    const nuevoRol = this.create(data);
+    return await this.save(nuevoRol);
   },
 
   async listarRoles() {
-    return await this.find({ order: { idRol: "ASC" } });
+    return await this.find({ 
+      where: { estado: true},
+      order: { idRol: "ASC" } 
+    });
+  },
+
+  async obtenerRolPorId(idRol: number) {
+    return await this.findOne({
+      where: { idRol, estado: true }
+    });
+  },
+
+  async actualizarRol(idRol: number, data: Partial<Rol>) {
+    await this.update(idRol, data);
+    return await this.obtenerRolPorId(idRol);
+  },
+
+  async eliminarRol(idRol: number) {
+    await this.update(idRol, { estado: false });
+    return true;
   }
 })
