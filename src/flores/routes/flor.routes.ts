c0122170b158c5bb713869d6abe76012a542bce0
PamelaCanaza <lunapamela886@gmail.com>
2025-11-24 00:00:31 -0400

    feat(flor): importar dtos y middleware a routes de flor

--- src/flores/routes/flor.routes.ts
+++ src/flores/routes/flor.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import { FlorController } from "../controllers/flor.controller";
+import { CrearFlorDto } from "../dtos/crearFlor.dto";
+import { ActualizarFlorDto } from "../dtos/actualizarFlor.dto";
+import { validarEntidad } from "../../middlewares/validarEntidad";
 
 const router = Router();
 
@@ -9,8 +12,8 @@ router.get("/color/:color", FlorController.buscarPorColor);
 router.get("/buscar", FlorController.buscarPorNombre);
 router.get("/:id", FlorController.obtenerPorId);
 
-router.post("/", FlorController.crearFlor);
-router.put("/:id", FlorController.actualizarFlor);
+router.post("/", validarEntidad(CrearFlorDto), FlorController.crearFlor);
+router.put("/:id", validarEntidad(ActualizarFlorDto), FlorController.actualizarFlor);
 router.delete("/:id", FlorController.eliminar);
 
 
