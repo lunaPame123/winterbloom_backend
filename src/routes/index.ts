331afa9a88746bb5946efc93cdf063730eb4a8fe
PamelaCanaza <lunapamela886@gmail.com>
2025-12-05 23:33:18 -0400

    feat: importacion de todas las rutas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,21 @@
 import { Router } from "express";
-import personaRoutes from "../personas/routes/persona.routes";
-import usuarioRoutes from "../usuarios/routes/usuario.routes";
-import rolRoutes from "../roles/routes/rol.routes";
+
+// Importar rutas
 import arregloRoutes from "../arreglos/routes/arreglo.routes";
 import florRoutes from "../flores/routes/flor.routes";
 import pedidoRoutes from "../pedidos/routes/pedido.routes";
+import personaRoutes from "../personas/routes/persona.routes";
+import rolRoutes from "../roles/routes/rol.routes";
+import usuarioRoutes from "../usuarios/routes/usuario.routes";
 
 const router = Router();
 
-router.use("/personas", personaRoutes);
-router.use("/usuarios", usuarioRoutes);
-router.use("/roles", rolRoutes);
+// Rutas principales
 router.use("/arreglos", arregloRoutes);
 router.use("/flores", florRoutes);
 router.use("/pedidos", pedidoRoutes);
+router.use("/personas", personaRoutes);
+router.use("/roles", rolRoutes);
+router.use("/usuarios", usuarioRoutes);
 
 export default router;
